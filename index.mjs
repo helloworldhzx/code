0aef2486c8ca3772616f12c06292da4bb6873896
hzx <657070781@qq.com>
2019-07-16 13:36:11 +0800

    2

--- index.mjs
+++ index.mjs
@@ -5,11 +5,56 @@
 // setTimeout(() => {
 //     console.log(name, hobbies); //Yvette ["coding", "writing"]
 // }, 500); //Yvette
-function print(n){
-    setTimeout(()=>{
-        console.log(n)
-    },Math.floor(Math.random()*1000))
+// function print(n){
+//     setTimeout(()=>{
+//         console.log(n)
+//     },Math.floor(Math.random()*1000))
+// }
+// for(var i=0;i<100;i++){
+//     print(i)
+// }
+
+// var a = [1,2,3];
+// a.join=a.shift
+// if(a == 1 && a == 2 && a == 3){
+//  	console.log(144);
+// }
+// var a = {
+//     b:1,
+//     toString:function(){
+//         return this.b++
+//     }
+// };
+// if(a == 1 && a == 2 && a == 3){
+//  	console.log(144);
+// }
+// function bc(){
+//     // return 123
+//     return new Promise((res1)=>{
+//         setTimeout(function(){res1()},10000)
+//     })
+// }
+// function *a(){
+//     const n = yield bc();
+//     console.log(n)
+//     yield 2;
+// }
+// var b =a()
+// console.log(b.next().value)
+// console.log(b.next())
+// console.log(b.next())
+// console.log(b.next())
+
+var length = 10 
+function fn(){
+console.log(this.length);
 }
-for(var i=0;i<100;i++){
-    print(i)
+var obj = {
+    length:5,
+    me:function(fn){
+        fn();
+        console.log(typeof arguments)
+        arguments[0]()
+    }
 }
+obj.me(fn,1)
