29c56760a1958d426de22c1d098c7e309e765147
hzx <657070781@qq.com>
2019-07-17 13:52:35 +0800

    444

--- name.mjs
+++ name.mjs
@@ -2,3 +2,16 @@ let name = 'William';
 setTimeout(() => { name = 'Yvette'; hobbies.push('writing'); }, 300);
 export { name };
 export var hobbies = ['coding'];
+
+function foo(){
+    "use strict";
+    err=1
+    try{
+        console.log("try")
+        throw "hello"
+
+    }catch(err){
+        console.log(err)
+    }
+    console.log(err)
+}
